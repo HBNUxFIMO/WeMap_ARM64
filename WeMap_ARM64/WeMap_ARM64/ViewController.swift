fcfe133011378d63dda42e98b1d084b525c89f05
giljihun <accc45@outlook.com>
2024-08-20 08:57:08 +0700

    Initial commit of WeMap_ARM64 project

---
+++ WeMap_ARM64/WeMap_ARM64/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeMap_ARM64
+//
+//  Created by 길지훈 on 8/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
